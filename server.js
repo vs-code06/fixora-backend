cb71f1ae1dd6b83b616aef89bf8b7cc81080549f
Vipul Sharma <vipultheplayer@gmail.com>
2025-11-12 18:13:45 +0530

    cors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
 app.use(
   cors({
-    origin: [FRONTEND_URL],
+    origin: 'https://fixora-frontend-zeta.vercel.app',
     credentials: true,
   })
 );
